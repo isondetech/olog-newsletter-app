a649f4438800e6118fff2c7b71aa049604f4e2f9
neosProton <protonneo@outlook.com>
2022-01-05 13:15:00 +0000

    upload and update newsletters
    Added an authentication process
    Users will now need a passcode to access certain pages

--- app.py
+++ app.py
@@ -1,30 +1,63 @@
-from datetime import date
-from os import link
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, flash
+from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import redirect
 from sqlalchemy import desc
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField
+from wtforms.validators import  InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '&_ux{2&4?GLQ8@y7'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return passcode.query.get(int(user_id))
 
 class newsletter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String)
     date =  db.Column(db.String)
 
+class passcode(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(80))
+
+class LoginForm(FlaskForm):
+    passcode = PasswordField(validators=[InputRequired()],render_kw={"placeholder":" Enter passcode"})
+    submit = SubmitField("Login")
+
+
 @app.route('/')
 def index():
     db_data = newsletter.query.order_by(desc(newsletter.id))
-    return render_template('home.html', db_data = db_data)
+    return render_template('home.html', db_data = db_data[:14])
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    user = passcode.query.get_or_404(2)
+    if form.validate_on_submit():
+        if bcrypt.check_password_hash(user.password, form.passcode.data):
+            login_user(user)
+            return redirect('/admin')
+        else:
+            flash("Wrong Passcode")
+    return render_template('login.html', form=form)
 
 @app.route('/admin', methods=['POST', 'GET'])
+@login_required
 def admin():
     if request.method == 'POST':
         newsletter_link = request.form['link']
@@ -35,9 +68,10 @@ def admin():
         return redirect('/admin')
     else:
         db_data = newsletter.query.order_by(desc(newsletter.id))
-        return render_template('admin.html', db_data = db_data)
+        return render_template('admin.html', db_data = db_data[:14])
 
 @app.route('/update/<int:id>', methods=["POST","GET"])
+@login_required
 def update(id):
     data = newsletter.query.get_or_404(id)
     if request.method == 'POST':
@@ -49,8 +83,10 @@ def update(id):
         return render_template('update.html', db_data=data)
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect("/login")
 
 if __name__ == "__main__":
     app.run(debug=True)
